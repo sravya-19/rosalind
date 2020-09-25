e2daea32489c7105b587489b47bc96e0e46258ad
sravya-ach <40864038+sravya-ach@users.noreply.github.com>
2020-09-26 02:55:00 +0530

    Delete DNA.py

--- DNA.py
+++
@@ -1,8 +0,0 @@
-
-input_location = 'downloads/rosalind_dna.txt'
-
-file = open(input_location).readlines()
-string = file[0].strip()
-
-print('%r %r %r %r' % (string.count('A'), string.count(
-    'C'), string.count('G'), string.count('T')))
